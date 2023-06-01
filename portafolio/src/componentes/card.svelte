2c45f8f18dfb25fef5155f79d78d791438c9f282
Teviets <est21405@uvg.edu.gt>
2023-05-31 19:19:25 -0600

    Finalizacion teorica de card

--- portafolio/src/componentes/card.svelte
+++ portafolio/src/componentes/card.svelte
@@ -1,7 +1,24 @@
 <script>
+    import { onMount } from 'svelte';
+
     export let nameProyect;
     export let img;
     export let description;
+    export let link;
+    export let width;
+    export let height;
+
+    onMount(() => {
+        if(width == undefined && height == undefined){
+            width = 100;
+            height = 100;
+        }
+        const buttonCard = document.getElementById('buttonCard');
+        buttonCard.style.width = width + 'px';
+        buttonCard.style.height = height + 'px';
+    });
+
+    
 
     let showDesc = false;
 
@@ -11,13 +28,15 @@
 </script>
 
 <button id="buttonCard" on:mouseenter={showDescription} on:mouseleave={showDescription}>
-    <img src={img} alt="">
-    {#if showDesc}
-        <div id="contentCard" data-aos="fade-left">
-            <h3>{nameProyect}</h3>
-            <p>{description}</p>
-        </div>
-    {/if}
+    <a href={link}>
+        <img src={img} alt="">
+        {#if showDesc}
+            <div id="contentCard" data-aos="fade-left">
+                <h3>{nameProyect}</h3>
+                <p>{description}</p>
+            </div>
+        {/if}
+    </a>
 </button>
 
 <style>
