00fd176c513a7895f6d2f251c22f4a0862ccefad
Teviets <est21405@uvg.edu.gt>
2023-05-31 22:19:13 -0600

    Laboratorios

--- portafolio/src/componentes/card.svelte
+++ portafolio/src/componentes/card.svelte
@@ -1,56 +1,58 @@
 <script>
-    import { onMount } from 'svelte';
-
     export let nameProyect;
     export let img;
     export let description;
     export let link;
     export let width;
     export let height;
-
-    onMount(() => {
-        if(width == undefined && height == undefined){
-            width = 100;
-            height = 100;
-        }
-        const buttonCard = document.getElementById('buttonCard');
-        buttonCard.style.width = width + 'px';
-        buttonCard.style.height = height + 'px';
-    });
-
-    
-
+  
     let showDesc = false;
-
-    function showDescription(){
-        showDesc = !showDesc;
+  
+    function showDescription() {
+      showDesc = !showDesc;
     }
-</script>
-
-<button id="buttonCard" on:mouseenter={showDescription} on:mouseleave={showDescription}>
-    <a href={link}>
-        <img src={img} alt="">
-        {#if showDesc}
-            <div id="contentCard" data-aos="fade-left">
+  </script>
+  
+  <button id="buttonCard" style="background-image: url({img}); background-size: cover; width: {width}px; height:{height}px;">
+    <a href={link} id="linkCard" on:mouseenter={showDescription} on:mouseleave={showDescription}>
+      {#if showDesc}
+        <div id="contentCard" data-aos="fade-left">
+            <div id="textoCard">
                 <h3>{nameProyect}</h3>
                 <p>{description}</p>
             </div>
-        {/if}
+        </div>
+      {/if}
     </a>
-</button>
-
-<style>
-    #buttonCard{
-        width: 100px;
-        height: 100px;
-        background-color: transparent;
-        border: none;
-        display: flex;
-        flex-direction: row;
+  </button>
+  
+  <style>
+    #buttonCard {
+      background-color: transparent;
+      border: none;
+      display: flex;
+      flex-direction: row;
     }
-    #contentCard{
+    #contentCard {
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(to left, transparent, #000);
+    }
+    #contenidoIMG {
+      width: 100%;
+    }
+    #linkCard {
+      text-decoration: none;
+      height: 100%;
+      width: 100%;
+    }
+    #contentCard.showDesc {
+      background-color: black;
+    }
+    #textoCard{
         width: 50%;
         height: 100%;
-        background: linear-gradient(right, transparent, #000);
+        color: white;
     }
-</style>
+  </style>
+  
--- portafolio/src/componentes/inicios.svelte
+++ portafolio/src/componentes/inicios.svelte
@@ -1,11 +1,68 @@
 <script>
+import CardLabs from './card.svelte';
 
+import Servidor from '../assets/img/servidor.png'
+import CSS from '../assets/img/css.png'
+import Chat from '../assets/img/chat.png'
+import Memoria from '../assets/img/memoria.png'
 </script>
 
-<div>
-    <h1>quivo</h1>
+<div id="IniciosContainer">
+    <h1>Laboratorios</h1>
+    <div id="cardsContainerInicio">
+        <div class="item">
+            <CardLabs 
+                nameProyect="Configurando un servidor" 
+                img={Servidor}
+                description="En este laboratorio se configuro un servidor con el sistema operativo Ubuntu Server 20.04 LTS, se configura el acceso remoto desde Windows 10. Esto es fue impactante para mi pues descubri un gusto por la utilizacion de cloud computing y me impulso a querer conocer mas sobre AWS."
+                link="#"
+                width="600"
+                height="300"
+                />
+        </div>
+        <div class="item">
+            <CardLabs 
+                nameProyect="Estilos CSS" 
+                img={CSS}
+                description="Este laboratorio consistio en realizar una copia de un diseño hecho con css de una pagina especifica y buscar implementar estilos un poco mas avanzados, en este laboratorio aprendimos la utiliza de @media y como poder hacer trancisiones desde css."
+                link="https://css-stw.web.app/"
+                width="600"
+                height="300"
+                />
+        </div>
+        <div class="item">
+            <CardLabs 
+                nameProyect="Javascript" 
+                img={Chat}
+                description="En este laboratorio aprendiamos sobre la utilizacion de javascript y de la potencia que tiene este lenguaje de programacion, llegamos diseñar nuestro propio chat de la clase, haciendo un fetch a una instancia en mongo diseñada por el catedratico. Tambien llegamos a diseñar la pagina completamente desde javascript."
+                link="https://chatstw.web.app/"
+                width="600"
+                height="300"
+                />
+        </div>
+        <div class="item">
+            <CardLabs 
+                nameProyect="Memoria React" 
+                img={Memoria}
+                description="En este laboratorio implementamos el framework React con webpack, para poder diseñar un juego de memoria, en el cual se utilizo diversas librerias que facilitaron de gran manera la creacion de los diversos componente y permitiendo que estos sean reactivos a lo que usuario esta empleando en el juego."
+                link="https://lab-8-fd228.web.app/"
+                width="600"
+                height="300"
+                />
+        </div>
+    </div>
 </div>
 
 <style>
+    #cardsContainerInicio {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-template-rows: repeat(2, 1fr);
+        gap: 10px;
+    }
 
+    .item {
+        background-color: transparent;
+        padding: 10px;
+    }
 </style>
